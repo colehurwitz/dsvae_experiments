ae6cfbb43638a54404a1a05692e19ae0ef54dc93
root <root@virtualserver01.akash-srivastava-s-Account.cloud>
2020-03-12 23:05:24 +0000

    update plot

--- utils/plotting_utils.py
+++ utils/plotting_utils.py
@@ -4,7 +4,7 @@ import numpy as np
 from misc import merge
 
 def plot_pytorch_images(images, num_images=5, title="", xticks=False, yticks=False):
-    images.detach()
+    images = images.detach()
     if images.is_cuda:
         images = images.cpu()
     images = images.permute(0,2,3,1)
